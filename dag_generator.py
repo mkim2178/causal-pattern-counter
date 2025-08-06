81cfdcc8a37975bc02ecf3e936cf5910c404c58f
mkim2178 <mkim2178@berkeley.edu>
2025-08-06 11:54:04 +0900

    both DAG generator now guarantees a connected DAG

--- dag_generator.py
+++ dag_generator.py
@@ -3,13 +3,12 @@ import random
 import string
 from exceptions import InvalidInputException
 
-
 ALPHABETS = list(string.ascii_uppercase)
 MIN_NODE = 3
 MAX_NODE = 26
 VALID_ORDERS = ["DESC", "ASC"]
 
-class DAG_Generator:
+class DagGenerator:
 
     def __init__(self):
         pass
@@ -23,6 +22,7 @@ class DAG_Generator:
         if not (MIN_NODE <= node_number <= MAX_NODE):
             raise InvalidInputException(f"INVALID RANGE: SHOULD BE BETWEEN {MIN_NODE} ~ {MAX_NODE}")
         
+
     def handle_invalid_order(self, order):
         
         if not isinstance(order, str) or isinstance(order, bool):
@@ -31,35 +31,26 @@ class DAG_Generator:
         if order not in VALID_ORDERS:
             raise InvalidInputException(f"INVALID STRING: SHOULD BE ONE OF: {VALID_ORDERS}")
 
-
     
-    def generate_random_dag(self, node_number):
+    def generate_connected_random_dag(self, node_number):
         """
-        This function creates an alphabetical random DAG.
-
-        1. Check if the number of node is valid.
-        2. Initialize a list of nodes by select 'node_number' random nodes from 'ALPHABETS'.
-        3. Initialize a random edge number (the possible edge number for a connected DAG will be between the node_number - 1 to (node_number * (node_number - 1)) // 2).
-        4. Initialize a networkx.DiGraph() object (a directed graph).
-        5. Run a while loop until the number of edges in the networkx.DiGraph() is equal to the 'random_edge_number'.
-        6. Randomly select two nodes from the list from the first step (source and target)
-        7. If there is no edge between source and target create an edge.
-        8. Check if there is a cycle in our current graph (use networkx.is_directed_acyclic_graph).
-        9. If a cycle exists, remove the edge that has been recently added.
-        10. After the while loop, return the networkx.DiGraph() object.
+        Generates a connected random DAG without enforcing a topological sequence.
         """
 
         self.handle_invalid_node_number(node_number)
 
-        alphabetical_nodes = random.sample(ALPHABETS, node_number)
+        random_nodes = random.sample(ALPHABETS, node_number)
         random_edge_number = random.randint(node_number - 1, (node_number * (node_number - 1)) // 2)
 
         G = nx.DiGraph()
 
-        while G.number_of_edges() < random_edge_number:
-            source, target = random.sample(alphabetical_nodes, 2)
+        for i in range(node_number - 1):
+            G.add_edge(random_nodes[i], random_nodes[i + 1])
 
-            if not G.has_edge(source, target):
+        while G.number_of_edges() < random_edge_number:
+            source, target = random.sample(random_nodes, 2)
+            
+            if not G.has_edge(source ,target):
                 G.add_edge(source, target)
                 if not nx.is_directed_acyclic_graph(G):
                     G.remove_edge(source, target)
@@ -67,21 +58,11 @@ class DAG_Generator:
         return G
 
 
-    def generate_random_topological_dag(self, node_number, order):
+    def generate_connected_random_dag_using_topological_order(self, node_number, order):
         """
-        This function creates a topological DAG.
+        Generates a connected random DAG by enforcing a topological sequence.
+        """
 
-        1. Check if the number of node and the order string is valid.
-        3. Initialize a list of 'node_number' nodes that has a descending order.
-        4. Create an empty list to store every possible edge
-        5. Check the order option -> if order is "ASC", re-initialize the node list into an ascending order.
-        6. Use a 2D for loop to add every possible edge with using a certain order.
-        7. Initialize a random edge number (the possible edge number for a connected DAG will be between the node_number - 1 to (node_number * (node_number - 1)) // 2).
-        8. Randomly select edges using the 'random_edge_number'.
-        9. Initialize a networkx.DiGraph() object (a directed graph).
-        10. Add all nodes and edges.
-        11. Return the networkx.DiGraph() object.
-        """
         self.handle_invalid_node_number(node_number)
         self.handle_invalid_order(order)
 
@@ -92,13 +73,14 @@ class DAG_Generator:
             numerical_nodes = [i for i in range(1, node_number + 1)]
 
         # guarantee a connected graph
+        # we will use this set when checking if a possible edge is already in the 'connected_edges' list, `not in set()`` is more efficient than `not in list()`
         connected_edges = [(numerical_nodes[i], numerical_nodes[i + 1]) for i in range(node_number - 1)]
-        connected_edges_set = set(connected_edges) # this will be more efficient when checking if a possible edge is already in the `conected_edges` list
+        connected_edges_set = set(connected_edges)
 
         for i in range(node_number):
             for j in range(i + 1, node_number):
                 possible_edge = (numerical_nodes[i], numerical_nodes[j])
-                if possible_edge not in connected_edges_set: # we're using set instead of list when using `in` so it would be more efficient if the length of connected_edges_set is big
+                if possible_edge not in connected_edges_set:
                     possible_edges.append(possible_edge)
 
         random_edge_number = random.randint(node_number - 1, (node_number * (node_number - 1)) // 2)
@@ -110,4 +92,3 @@ class DAG_Generator:
         G.add_edges_from(connected_edges + additional_edges)
 
         return G
-
