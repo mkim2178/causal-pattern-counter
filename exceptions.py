d7ba2dda0d93501c6c40a36a694ff042835e1636
mkim2178 <mkim2178@berkeley.edu>
2025-08-06 15:30:02 +0900

    add new exception class for the GraphVisualizer

--- exceptions.py
+++ exceptions.py
@@ -1,3 +1,7 @@
 class InvalidInputException(Exception):
     def __init__(self, message):
         super().__init__(message)
+    
+class MissingGraphException(Exception):
+    def __init__(self, message):
+        super().__init__(message)
