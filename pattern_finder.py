013eaa87ef5b2342be89ace760f179d45664cf2a
mkim2178 <mkim2178@berkeley.edu>
2025-08-06 11:55:17 +0900

    finding functions have been created. they find every possible forks and colliders with 3 nodes and 2 edges

--- pattern_finder.py
+++ pattern_finder.py
@@ -1,3 +1,38 @@
-class Pattern_Finder:
+import networkx as nx
 
-    pass
+class PatternFinder:
+
+    def __init__(self):
+        pass
+
+    
+    def three_node_colliders(self, DAG):
+
+        colliders = list(nx.dag.colliders(DAG))
+
+        if len(colliders) == 0:
+            return []
+    
+        collider_graphs = []
+
+        for collider in colliders:
+            collider_graph = nx.DiGraph()
+            collider_graph.add_edges_from([(collider[0], collider[1]), (collider[2], collider[1])])
+            collider_graphs.append(collider_graph)
+    
+        return collider_graphs
+    
+
+    def three_node_forks(self, DAG):
+
+        fork_graphs = []
+
+        for node in list(DAG.nodes):
+            child_nodes = list(nx.descendants_at_distance(DAG, node, 1))
+            for i in range(len(child_nodes)):
+                for j in range(i + 1, len(child_nodes)):
+                    fork = nx.DiGraph()
+                    fork.add_edges_from([(node, child_nodes[i]), (node, child_nodes[j])])
+                    fork_graphs.append(fork)
+        
+        return fork_graphs
