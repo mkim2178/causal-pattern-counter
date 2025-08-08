b28ca1c1694c76c8a713ca589b60cf0234a3e312
mkim2178 <mkim2178@berkeley.edu>
2025-08-08 11:01:12 +0900

    indepedency_finder class will include functions that checks the conditional independency with two specific nodes. currently we have two functions: first one returns source and target nodes that are not adjacent with each other. other one returns every possible paths even they are not ideal from directed graph.

---
+++ independency_finder.py
@@ -0,0 +1,31 @@
+import random
+import networkx as nx
+
+
+
+class IndependencyFinder:
+
+    
+    def select_random_source_and_target(self, DAG):
+        """
+        This function returns source and target nodes that are not adjacent with each other.
+        """
+
+        source = target = None
+        is_adjacent = True
+
+        while is_adjacent:
+            source, target = random.sample(list(DAG.nodes), 2)
+            if not DAG.has_edge(source, target) and not DAG.has_edge(target, source):
+                is_adjacent = False
+        
+        return source, target
+
+
+    def get_all_paths(self, DAG, source, target):
+        """
+        This function returns all possible paths between source and target nodes.
+        """
+
+        return list(nx.all_simple_paths(DAG.to_undirected(), source, target))
+    
