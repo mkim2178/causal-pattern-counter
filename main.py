063ea740ba95dc76cbe7676624ec862e6862fbc2
mkim2178 <mkim2178@berkeley.edu>
2025-08-11 11:56:27 +0900

    organized debugging codes

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from dag_generator import DagGenerator
 from pattern_finder import PatternFinder
 from graph_visualizer import GraphVisualizer
 from independency_finder import IndependencyFinder
-
+from itertools import combinations
 
 
 
@@ -19,30 +19,57 @@ def main():
     independency_finder = IndependencyFinder()
 
 
-    DAG = dag_generator.generate_connected_random_dag(26)
-    print(DAG.number_of_edges())
 
-    colliders = pattern_finder.three_node_colliders(DAG)
-    forks = pattern_finder.three_node_forks(DAG)
-
-    source, target = independency_finder.select_random_source_and_target(DAG)
-    print(nx.find_minimal_d_separator(DAG, source, target))
-
-    print(source, target)
+    """
+    ---------------------------------------TESTING A GENERATOR---------------------------------------
+    """
 
 
+    random_dag = dag_generator.generate_connected_random_dag(5)
+
+    for node in list(random_dag.nodes):
+        print(f"node: {node}, descendants with distance 2: {list(nx.descendants_at_distance(random_dag, node, 2))}")
+        descendants_with_distance_two = list(nx.descendants_at_distance(random_dag, node, 2))
+
+        for descendant in descendants_with_distance_two:
+            print(list(nx.all_shortest_paths(random_dag, node, descendant)))
+        
+
+    colls = pattern_finder.find_all_colliders(random_dag)
+    forks = pattern_finder.find_all_forks(random_dag)
+
+    # print(colls)
+    # print(forks)
+
+    nx.draw(random_dag, with_labels=True)
+    plt.show()
 
 
 
 
+    # topological_random_dag = dag_generator.generate_connected_random_dag_using_topological_order(15, "DESC")
+    """
+    ---------------------------------------TESTING A GENERATOR---------------------------------------
+    """
+
+
+
+    """
+    ---------------------------------------TESTING A PATTERN_FINDER---------------------------------------
+    """
+    # colliders = pattern_finder.three_node_colliders(random_dag)
+    # forks = pattern_finder.three_node_forks(random_dag)
+    """
+    ---------------------------------------TESTING A PATTERN_FINDER---------------------------------------
+    """
 
 
 
     """
     ---------------------------------------TESTING A VISUALIZER---------------------------------------
     """
-    graph_visualizer.visualize_single_graph(DAG)
-    graph_visualizer.visualize_multiple_graphs(colliders, "Collider")
+    # graph_visualizer.visualize_single_graph(random_dag)
+    # graph_visualizer.visualize_multiple_graphs(colliders, "Collider")
     # graph_visualizer.visualize_multiple_graphs(forks, "Fork", "lightgreen")
     """
     ---------------------------------------TESTING A VISUALIZER---------------------------------------
@@ -51,10 +78,13 @@ def main():
 
 
 
-
-
-
-
+    """
+    ---------------------------------------TESTING AN INDEPEDENCY_FINDER---------------------------------------
+    """
+    # source, target = independency_finder.select_random_source_and_target(DAG)
+    """
+    ---------------------------------------TESTING AN INDEPEDENCY_FINDER---------------------------------------
+    """
 
 
 
