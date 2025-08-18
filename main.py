8ab685ac9cb72a132e0e7d2fcd994dcef8251958
mkim2178 <mkim2178@berkeley.edu>
2025-08-18 20:15:44 +0900

    testing various possible parameters for our new RandomDagGenerator class

--- main.py
+++ main.py
@@ -1,90 +1,49 @@
+
 import networkx as nx
 import matplotlib.pyplot as plt
-import random
-import time
-import math
-from dag_generator import DagGenerator
-from pattern_finder import PatternFinder
-from graph_visualizer import GraphVisualizer
-from independency_finder import IndependencyFinder
-from itertools import combinations
+from graph_tools import RandomDagGenerator
 
 
 
 def main():
 
-    dag_generator = DagGenerator()
-    pattern_finder = PatternFinder()
-    graph_visualizer = GraphVisualizer()
-    independency_finder = IndependencyFinder()
-
-
-
     """
-    ---------------------------------------TESTING A GENERATOR---------------------------------------
+    4
+    4, 3
+    4, 3, asc
+    4, 3, desc
+    4, asc
+    4, desc
+    
+    
+    
+    
     """
+    test_every_possible_parameter = [(4,), (4, 3), (4, 3, "asc"), (4, 3, "desc")]
 
+    for parameter in test_every_possible_parameter:
 
-    random_dag = dag_generator.generate_connected_random_dag(5)
-
-    for node in list(random_dag.nodes):
-        print(f"node: {node}, descendants with distance 2: {list(nx.descendants_at_distance(random_dag, node, 2))}")
-        descendants_with_distance_two = list(nx.descendants_at_distance(random_dag, node, 2))
-
-        for descendant in descendants_with_distance_two:
-            print(list(nx.all_shortest_paths(random_dag, node, descendant)))
-        
-
-    colls = pattern_finder.find_all_colliders(random_dag)
-    forks = pattern_finder.find_all_forks(random_dag)
-
-    # print(colls)
-    # print(forks)
-
-    nx.draw(random_dag, with_labels=True)
-    plt.show()
+        gen = RandomDagGenerator(*(parameter))
+        g = gen.generate_graph()
+    
 
 
 
 
-    # topological_random_dag = dag_generator.generate_connected_random_dag_using_topological_order(15, "DESC")
-    """
-    ---------------------------------------TESTING A GENERATOR---------------------------------------
-    """
+    random_dag_gen2 = RandomDagGenerator(4, order="asc")
+    G2 = random_dag_gen2.generate_graph()
+
+    random_dag_gen3 = RandomDagGenerator(4, order="desc")
+    G3 = random_dag_gen3.generate_graph()
 
 
+    # pos = nx.spring_layout(G, seed=42)
+    # nx.draw(G, pos=pos, with_labels=True)
+    # plt.show()
 
-    """
-    ---------------------------------------TESTING A PATTERN_FINDER---------------------------------------
-    """
-    # colliders = pattern_finder.three_node_colliders(random_dag)
-    # forks = pattern_finder.three_node_forks(random_dag)
-    """
-    ---------------------------------------TESTING A PATTERN_FINDER---------------------------------------
-    """
+    print("NO EXCEPTION")
 
 
-
-    """
-    ---------------------------------------TESTING A VISUALIZER---------------------------------------
-    """
-    # graph_visualizer.visualize_single_graph(random_dag)
-    # graph_visualizer.visualize_multiple_graphs(colliders, "Collider")
-    # graph_visualizer.visualize_multiple_graphs(forks, "Fork", "lightgreen")
-    """
-    ---------------------------------------TESTING A VISUALIZER---------------------------------------
-    """
-
-
-
-
-    """
-    ---------------------------------------TESTING AN INDEPEDENCY_FINDER---------------------------------------
-    """
-    # source, target = independency_finder.select_random_source_and_target(DAG)
-    """
-    ---------------------------------------TESTING AN INDEPEDENCY_FINDER---------------------------------------
-    """
 
 
 
