f1c4c4752c14047364909ceedbe7c896dedcf456
koreanwizard <harry992178@gmail.com>
2025-08-19 13:43:43 +0900

    still working on the ui. removed previous debugging codes and add columns to visualize two plots: one is created by pyplot, one is created by plotly. it's for comparing purpose. one of them will be removed

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 import streamlit as st
 import networkx as nx
 import plotly.graph_objects as go
-from graph_tools import RandomDagGenerator
+import matplotlib.pyplot as plt
+import graphviz
+from graph_tools import RandomDagGenerator, RandomDagVisualizer
 # App title
 st.title("Random Connected DAG Visualizer")
+
 
 LAYOUT = {"spring": nx.spring_layout,
           "spiral": nx.spiral_layout,
@@ -36,136 +39,50 @@ selected_layout = st.radio(label="Select the layout of graph",
                            )
     
 button = st.button(label="Create")
+col1, col2 = st.columns(2) # Creates two columns of equal width
 
 if button:
+
+
+    if selected_order is None:
+        dag_gen = RandomDagGenerator(node=node_number, edge=edge_number, order=None)
+    else:
+        dag_gen = RandomDagGenerator(node=node_number, edge=edge_number, order=ORDER[selected_order])
+    G = dag_gen.generate_graph()
+
+    dag_vis = RandomDagVisualizer(G, selected_layout)
+
+    with col1:
+        fig = dag_vis.visualize()
+        st.subheader("Plot with plotly")
+        st.plotly_chart(fig, use_container_width=True)
+
+    with col2:
+        st.subheader("Plot with pyplot")
+        dag_vis.visualize_with_pyplot()
+        st.pyplot(plt)
+
     st.write(node_number, edge_number, selected_order, selected_layout)
 
-    dag_gen = RandomDagGenerator(node=node_number, edge=edge_number, order=ORDER[selected_order])
-    G = dag_gen.generate_graph()
-    pos = LAYOUT[selected_layout](G)
-
-    edge_x = []
-    edge_y = []
-
-    for u, v in G.edges():
-        ux, uy = pos[u]
-        vx, vy = pos[v]
-        edge_x += [ux, vx, None]
-        edge_y += [uy, vy, None]
+    # Create a directed graph
 
 
-    edge_trace = go.Scatter(
-    x=edge_x, y=edge_y,
-    line=dict(width=1, color='skyblue'),
-    hoverinfo='none',
-    mode='lines')
 
-    node_x = []
-    node_y = []
 
-    for node in G.nodes():
-        x, y = pos[node]
-        node_x += [x]
-        node_y += [y]
+
     
-
-
-    node_trace = go.Scatter(
-    x=node_x, y=node_y,
-    mode='markers+text',
-    text=[str(node) for node in G.nodes()],
-    textposition="top center",
-    hoverinfo='text',
-    marker=dict(
-        size=30,
-        color='green',
-        line=dict(width=2, color='gray')),
-    textfont=dict(
-        size=17,
-        color="yellow"
-    )
-    )
-
-
-    fig = go.Figure(data=[edge_trace, node_trace],
-                layout=go.Layout(
-                    showlegend=False,
-                    hovermode='closest',
-                    margin=dict(l=0,r=0,t=0,b=0),
-                    xaxis=dict(showgrid=False, zeroline=False, showticklabels=False),
-                    yaxis=dict(showgrid=False, zeroline=False, showticklabels=False)
-                ))
-    
-
-    fig.update_layout(
-    autosize=True,         # let Plotly auto-adjust figure size
-    margin=dict(l=0,r=0,t=0,b=0)
-    )
-
-    st.plotly_chart(fig, use_container_width=True)
+   
 
 
 
+    # # Create a graphlib graph object
+    # graph = graphviz.Digraph()
 
+    # for u, v in G.edges():
+    #     graph.edge(str(u), str(v))
 
+    # st.graphviz_chart(graph)
 
-# # Generate random graph
+    # dag_gen = RandomDagGenerator(node=node_number, edge=edge_number, order=ORDER[selected_order])
+    # G = dag_gen.generate_graph()
 
-
-# generator = DagGenerator()
-# G = generator.generate_connected_random_dag(node_number)
-# pos = nx.spring_layout(G, seed=42)
-
-# # Extract edges
-# edge_x, edge_y = [], []
-# for u, v in G.edges():
-#     x0, y0 = pos[u]
-#     x1, y1 = pos[v]
-#     edge_x += [x0, x1, None]
-#     edge_y += [y0, y1, None]
-
-# edge_trace = go.Scatter(
-#     x=edge_x, y=edge_y,
-#     mode="lines",
-#     line=dict(width=1, color="gray"),
-#     hoverinfo="none"
-# )
-
-# # Extract nodes
-# node_x, node_y = zip(*[pos[node] for node in G.nodes()])
-# node_trace = go.Scatter(
-#     x=node_x, y=node_y,
-#     mode="markers",
-#     marker=dict(size=10, color="skyblue"),
-#     text=[f"Node {n}" for n in G.nodes()],
-#     hoverinfo="text"
-# )
-
-# # Combine into figure
-# fig = go.Figure(data=[edge_trace, node_trace],
-#                 layout=go.Layout(
-#                     showlegend=False,
-#                     hovermode="closest"
-#                 ),
-            
-                
-                
-#                 )
-
-
-# for u, v in G.edges():
-#     x0, y0 = pos[u]
-#     x1, y1 = pos[v]
-#     fig.add_annotation(
-#         ax=x0, ay=y0,
-#         x=x1, y=y1,
-#         xref="x", yref="y", axref="x", ayref="y",
-#         showarrow=True,
-#         arrowhead=3,
-#         arrowsize=1,
-#         arrowwidth=1,
-#         arrowcolor="gray"
-#     )
-
-# # Show in Streamlit
-# st.plotly_chart(fig, use_container_width=True)
