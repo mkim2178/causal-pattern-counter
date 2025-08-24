42a61a0326bf4b2185cbf2a3b42aed02b22f7e50
mkim2178 <mkim2178@berkeley.edu>
2025-08-24 14:16:34 +0900

    will just use streamlit's pyplot function to visualize the graph. new streamlit's radio has been added. user can select the layout of the graph.

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
 import streamlit as st
 import networkx as nx
-import plotly.graph_objects as go
 import matplotlib.pyplot as plt
-import graphviz
-from graph_tools import RandomDagGenerator, RandomDagVisualizer
+import plotly.graph_objects as go
+
 # App title
 st.title("Random Connected DAG Visualizer")
 
@@ -21,62 +20,65 @@ ORDER = {"descending": "desc",
 
 
 node_number = st.number_input(label="Number of Nodes (Required)", min_value=3, max_value=26, step=1)
-edge_number = st.number_input(label="Number of Edges (Optional)", min_value=node_number - 1, max_value=(node_number * (node_number - 1))//2, step=1)
+edge_number = st.number_input(label="Number of Edges (Optional)", value=None, min_value=node_number - 1, max_value=(node_number * (node_number - 1))//2, step=1)
 is_topological = st.checkbox(label="Absolute Topological")
 
 
-if not is_topological and "topological_order" in st.session_state:
-    st.session_state.topological_order = None
 
-selected_order = st.radio(label="Select the order of the topological sort",
-                          options=["descending", "ascending"],
-                          key="topological_order",
-                          disabled=not is_topological
-                          )
-
-selected_layout = st.radio(label="Select the layout of graph",
-                           options=["spring", "spiral", "circular", "random"]
-                           )
+order = st.radio(label="Select the order of the topological sort.", options=["descending", "ascending"], index=None, disabled=not is_topological, key="topological_order")
     
 button = st.button(label="Create")
 col1, col2 = st.columns(2) # Creates two columns of equal width
 
 if button:
-
-
-    if selected_order is None:
-        dag_gen = RandomDagGenerator(node=node_number, edge=edge_number, order=None)
-    else:
-        dag_gen = RandomDagGenerator(node=node_number, edge=edge_number, order=ORDER[selected_order])
-    G = dag_gen.generate_graph()
-
-    dag_vis = RandomDagVisualizer(G, selected_layout)
-
-    with col1:
-        fig = dag_vis.visualize()
-        st.subheader("Plot with plotly")
-        st.plotly_chart(fig, use_container_width=True)
-
-    with col2:
-        st.subheader("Plot with pyplot")
-        dag_vis.visualize_with_pyplot()
-        st.pyplot(plt)
-
-    st.write(node_number, edge_number, selected_order, selected_layout)
-
-    # Create a directed graph
+    st.write(node_number, edge_number, order)
 
 
 
 
-
-    
-   
+# # Generate random graph
 
 
+# generator = DagGenerator()
+# G = generator.generate_connected_random_dag(node_number)
+# pos = nx.spring_layout(G, seed=42)
 
-    # # Create a graphlib graph object
-    # graph = graphviz.Digraph()
+# # Extract edges
+# edge_x, edge_y = [], []
+# for u, v in G.edges():
+#     x0, y0 = pos[u]
+#     x1, y1 = pos[v]
+#     edge_x += [x0, x1, None]
+#     edge_y += [y0, y1, None]
+
+# edge_trace = go.Scatter(
+#     x=edge_x, y=edge_y,
+#     mode="lines",
+#     line=dict(width=1, color="gray"),
+#     hoverinfo="none"
+# )
+
+# # Extract nodes
+# node_x, node_y = zip(*[pos[node] for node in G.nodes()])
+# node_trace = go.Scatter(
+#     x=node_x, y=node_y,
+#     mode="markers",
+#     marker=dict(size=10, color="skyblue"),
+#     text=[f"Node {n}" for n in G.nodes()],
+#     hoverinfo="text"
+# )
+
+# # Combine into figure
+# fig = go.Figure(data=[edge_trace, node_trace],
+#                 layout=go.Layout(
+#                     showlegend=False,
+#                     hovermode="closest"
+#                 ),
+            
+                
+                
+#                 )
+
 
     # for u, v in G.edges():
     #     graph.edge(str(u), str(v))
